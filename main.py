a00d5e5c52c9af2cca43efdb2630f9272a8ec79f
Rasa Chekeni <chekani.rasa@gmail.com>
2025-04-17 17:58:18 +0330

    Feat: V-0.3.0 Implemented compute_basis Function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+from itertools import combinations_with_replacement
 
 
 def load_dataset():
@@ -32,9 +33,25 @@ def split_data(x, y, train_ratio):
     return x[train_idx], y[train_idx], x[test_idx], y[test_idx]
 
 
+def compute_basis(dataset, degree):
+    n_samples, n_features = dataset.shape
+    phi = np.ones((n_samples, 1))
+    for d in range(1, degree + 1):
+        combs_d = combinations_with_replacement(range(n_features), d)
+        for comb in combs_d:
+            new_feature = np.prod(dataset[:, comb], axis=1, keepdims=True)
+            phi = np.hstack((phi, new_feature))
+    return phi
+
+
 def main():
     x, y = load_dataset()
     x_train, y_train, x_test, y_test = split_data(x, y, 0.8)
+
+    degrees = range(1, 6)
+    for degree in degrees:
+        phi_train = compute_basis(x_train, degree)
+        phi_test = compute_basis(x_test, degree)
 
 
 if __name__ == "__main__":
